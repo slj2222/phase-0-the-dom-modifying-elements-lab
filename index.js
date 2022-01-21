b32d49249d683bbe0e5f2dd55b3576cd7b89d482
Spencer Johnson <sljohnson2222@gmail.com>
2022-01-21 08:36:19 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.textContent = "Spencer is the champion";
+document.body.append(newHeader);
