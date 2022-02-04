7c37017852dbba076144d6520684b92512cbdcf2
Spencer Johnson <sljohnson2222@gmail.com>
2022-02-04 09:41:04 -0600

    commit tree 194c056e3f67504b105c7ae17f4325e23d8ed9cd

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-// Write your code here!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const element = document.getElementById("main");
 element.remove();
 
-const newHeader = document.createElement("h1");
-newHeader.id = "victory";
+// const newHeader = document.createElement("h1");
+// newHeader.id = "victory";
 
-newHeader.textContent = "Spencer is the champion";
-document.body.append(newHeader);
+// newHeader.textContent = "Spencer is the champion";
+// document.body.append(newHeader);
